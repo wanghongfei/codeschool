c1aef43188b30874b4c179594517dcbcf42b429b
wanghongfei <brucew@vip.qq.com>
2014-07-08 21:03:43 +0800

    修复用户logout时，对应的session没有被从sessionList中删除的问题

--- src/main/java/cn/fh/codeschool/servlet/LoggedInUserCollection.java
+++ src/main/java/cn/fh/codeschool/servlet/LoggedInUserCollection.java
@@ -9,15 +9,22 @@ import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import cn.fh.codeschool.model.Member;
 import cn.fh.codeschool.scope.ConversationManager;
 
 public class LoggedInUserCollection implements HttpSessionListener {
+	private static Logger logger = LoggerFactory.getLogger(LoggedInUserCollection.class);
+
 	// 保存已登陆用户的session
 	private static Map<String, HttpSession> sessionMap = new HashMap<String, HttpSession>();
 	
 	// 保存所有session
 	private static List<HttpSession> sessionList = new ArrayList<HttpSession>();
+	
+	
 
 	@Override
 	public void sessionCreated(HttpSessionEvent event) {
@@ -30,11 +37,23 @@ public class LoggedInUserCollection implements HttpSessionListener {
 	 */
 	@Override
 	public void sessionDestroyed(HttpSessionEvent event) {
-		HttpSession session = event.getSession();
+		HttpSession session = event.getSession(); // This is a copy of the original session????
 		Member m = (Member)session.getAttribute("currentUser");
 		
 		if (null != m) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("## User session '{}' destroyed!", m.getUsername());
+			}
 			sessionMap.remove(m.getUsername());
+			
+			// Remove this session from all session map.
+			int ix = 0;
+			for (int LEN = sessionList.size() ; ix < LEN ; ++ix) {
+				if (sessionList.get(ix).getId().equals(session.getId())) {
+					break;
+				}
+			}
+			sessionList.remove(ix);
 		}
 	}
 	
